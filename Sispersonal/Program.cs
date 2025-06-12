394b26517f80ec1d80b29d1c9ed331422d7f1e21
Paulo Srur <paulosrur777@gmail.com>
2025-06-12 11:19:58 -0300

    clase empleado y enum de cargo

---
+++ Sispersonal/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SisPersonal;
+
+public enum Cargo
+{
+    Auxiliar,
+    Administrativo,
+    Ingeniero,
+    Especialista,
+    Investigador
+}
+
+public class Empleado
+{
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+    public char EstadoCivil { get; set; } // 'C' para Casado, 'S' para Soltero
+    public DateTime FechaIngreso { get; set; }
+    public double SueldoBasico { get; set; }
+    public Cargo Cargo { get; set; }
+
+    // Constructor
+    public Empleado(string nombre, string apellido, DateTime nacimiento, char estadoCivil, DateTime ingreso, double sueldo, Cargo cargo)
+    {
+        Nombre = nombre;
+        Apellido = apellido;
+        FechaNacimiento = nacimiento;
+        EstadoCivil = estadoCivil;
+        FechaIngreso = ingreso;
+        SueldoBasico = sueldo;
+        Cargo = cargo;
+    }
+}
